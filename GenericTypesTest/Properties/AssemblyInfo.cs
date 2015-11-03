91c6a91baf57937014ba12d006c3a0f7c283f754
Paco Perez <fperez@Telecinco.es>
2015-11-03 10:56:40 +0100

    Generic Parameters

---
+++ GenericTypesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GenericTypesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mediaset España")]
+[assembly: AssemblyProduct("GenericTypesTest")]
+[assembly: AssemblyCopyright("Copyright © Mediaset España 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("edc0b77a-43ef-4618-9399-fb7942368256")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GenericTypesTest/TestGenericsTypes.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+ * Program to test how works generic types in C#
+ * 
+ */
+
+namespace GenericTypesTest
+{
+    class TestGenericsTypes
+    {
+        private Boss bossy;
+
+        public Boss Bossy
+        {
+            get { return bossy; }
+            set { bossy = value; }
+        }
+
+        
+        private List<Employee> employees = new List<Employee>();
+
+        public List<Employee> Employees
+        {
+            get { return employees; }
+            set { employees = value; }
+        }
+
+        static void Main(string[] args)
+        {
+            TestGenericsTypes program = new TestGenericsTypes();
+            Employee e = program.generateEmployee("Paco Pérez", 41);
+            program.genericToString<Employee>("Employee:", ref e);
+            Boss b = program.generateBoss("Javer Pérez", 50);
+            program.genericToString<Boss>("Boss:", ref b);
+            program.toStringJerarquia(program.Bossy);
+            
+        }
+
+        public Employee generateEmployee(string name, int age )
+        {
+            Employee employee = new Employee(name, age);
+            Employees.Add(employee);
+            //Console.WriteLine(String.Format("Empleado: {0}", employee.ToString()));
+            return employee;
+
+        }
+
+        public Boss generateBoss(string name, int age) 
+        {
+            Boss boss = new Boss(name, age);
+            Bossy = boss;
+            if (Employees != null)
+            {
+                Bossy.Employees = Employees;
+            }
+            return boss;
+            //Console.WriteLine(String.Format("Jefe: {0}", boss.ToString()));
+        }
+
+        public void toStringJerarquia(Boss boss) 
+        {
+            Console.WriteLine(String.Format("Orgranigrama: \n {0}", boss.ToString()));
+        }
+
+        /*
+         * Method wich recive generic type
+         */
+        public void genericToString<T>(string message, ref T t) 
+        {
+            Console.WriteLine(message + " {0}", t.ToString());
+        }
+    }
+}
